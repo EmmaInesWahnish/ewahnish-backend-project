9a206c8b6f08a9b91486d33e20a8d504e1253ba9
Emma Wahnish <ewahnish@gmail.com>
2022-07-03 17:17:42 -0300

    in preparation

--- project/src/routers/routerCart.js
+++ project/src/routers/routerCart.js
@@ -1,17 +1,15 @@
 import express from 'express';
 const routerCart = express.Router();
 
-let isAdmin = true;
-
 // *** ROUTES ***
 //This route returns the Cart list
-routerCart.get('/', async (req, res) => {
+routerCart.get('/:id/productos', async (req, res) => {
     try {
         const array = await Cart.getAll();
-        res.send({ message: 'Carritos ', carrito: array });
+        res.json({ message: 'Carritos ', carrito: array });
     }
     catch (error) {
-        res.send({
+        res.json({
             message: 'No se ha podido recuperar la lista de carritos',
             error: error
         })
@@ -25,66 +23,61 @@ routerCart.get('/:id', async (req, res) => {
         try {
             const carrito = await Cart.getById(id);
             if (carrito != undefined) {
-                res.send({
+                res.json({
                     message: 'carrito encontrado',
                     product: carrito
                 })
             } else {
-                res.send({
+                res.json({
                     message: "carrito no encontrado"
                 })
             }
         }
         catch (error) {
-            res.send({
+            res.json({
                 message: "Se produjo un error al buscar el carrito",
                 error: error
             })
         }
     } else {
-        res.send({
+        res.json({
             "error": "El id solicitado no es numerico"
         })
     }
 })
 
-//This route ads a product
+//This route ads an empty cart
 routerCart.post('/', async (req, res) => {
     let receive = req.body;
     let carrito = [{
         timestamp: receive.timestamp,
-        nombre: receive.nombre,
-        descripcion: receive.descripcion,
-        codigo: receive.codigo,
-        foto: receive.foto,
-        precio: receive.precio,
-        stock: receive.stock
+        productos: [],
     }]
     if (carrito) {
         try {
             await Cart.save(carrito);
             try {
                 const Cart = await Cart.getAll();
-                res.send({
+                res.json({
                     message: "carrito incorporado",
                     product: carrito
                 })
             }
             catch (error) {
-                res.send({
+                res.json({
                     message: 'No se ha podido obtener la lista de carritos',
                     error: error
                 })
             }
         }
         catch (error) {
-            res.send({
+            res.json({
                 message: 'No se ha podido guardar el carrito',
                 error: error
             })
         }
     } else {
-        res.send({
+        res.json({
             message: "Los datos suministrados son incorrectos"
         })
     }
@@ -92,70 +85,76 @@ routerCart.post('/', async (req, res) => {
 
 //This route updates the product with the selected id
 //A property is updated only if it receives a non null value
-routerCart.put('/:id', async (req, res) => {
+routerCart.post('/:id/productos', async (req, res) => {
     const id = parseInt(req.params.id);
     let receive = req.body;
     let searchedProduct = {};
     console.log("The id ", id, "receive  ", receive)
     try {
         const Cart = await Cart.getAll();
-        const index = Cart.findIndex(element => element.id === id);
-        searchedProduct = Cart[index];
-        console.log(index, " Modifico ", searchedProduct);
-        if (index !== -1) {
-
-            if (receive.title !== null && receive.title !== undefined) {
-                Cart[index].title = receive.title;
+        const indexc = Cart.findIndex(element => element.id === id);
+        searchedProduct = Cart[indexc];
+        console.log(indexc, " Modifico ", searchedProduct);
+        if (indexc !== -1) {
+            const theLength = searchedCart.productos.length
+            if (theLength > 0) {
+                const indexp = searchedCart.productos.findindex(element => element.id === receive.id);
+                Cart[indexc].productos[indexp].cantidad = Cart[indexc].productos[indexp].cantidad + receive.cantidad;
+            } else {
+                newProduct.id = receive.producto.id;
+                newProduct.timestamp = receive.producto.timestamp;
+                newProduct.nombre = receive.pronombre;
+                newProduct.descripcion = receive.descripcion;
+                newProduct.codigo = receive.codigo;
+                newProduct.foto = receive.foto;
+                newProduct.precio = receive.precio;
+                newProduct.stock = receive.stock;
+                newProduct.cantidad = receive.cantidad;
             }
-            if (receive.price !== null && receive.price !== undefined) {
-                Cart[index].price = receive.price;
-            }
-            if (receive.thumbnail !== null && receive.thumbnail !== undefined) {
-                Cart[index].thumbnail = receive.thumbnail;
-            }
+        
+            Cart.productos.push(newProduct);
 
             //The array gets updated here
             let array = [];
 
             Cart.forEach((element) => {
                 array.push({
-                    title: element.title,
-                    price: element.price,
-                    thumbnail: element.thumbnail
+                    timestamp: element.timestamp,
+                    productos: element.productos,
                 })
             })
 
-            //Cart.txt file is replaced with the updated array
+            //carrito.txt file is replaced with the updated array
             try {
-                await fs.promises.unlink('./src/files/Cart.txt');
+                await fs.promises.unlink('./src/files/carrito.txt');
                 try {
                     await Cart.save(array);
-                    res.send({
+                    res.json({
                         message: 'Modificacion exitosa',
                         product: array
                     })
                 }
                 catch (error) {
-                    res.send({
+                    res.json({
                         message: 'No fue posible cargar los carritos en Cart.txt',
                         error: error
                     })
                 }
             }
             catch (error) {
-                res.send({
-                    message: 'No se pudo borrar el archivo Cart.txt',
+                res.json({
+                    message: 'No se pudo borrar el archivo carrito.txt',
                     error: error
                 })
             }
         } else {
-            res.send({
+            res.json({
                 message: 'carrito no encontrado'
             })
         }
     }
     catch (error) {
-        res.send({
+        res.json({
             message: 'Ha ocurrido un error al intentar recuperar la lista de carritos',
             error: error
         })
@@ -163,7 +162,84 @@ routerCart.put('/:id', async (req, res) => {
 
 })
 
-//This route removes the product with the selected id
+routerCart.delete('/:id/productos', async (req, res) => {
+    const id = parseInt(req.params.id);
+    let receive = req.body;
+    let searchedProduct = {};
+    console.log("The id ", id, "receive  ", receive)
+    try {
+        const Cart = await Cart.getAll();
+        const indexc = Cart.findIndex(element => element.id === id);
+        searchedCart = Cart[indexc];
+        console.log(indexc, "  ", searchedProduct);
+        if (indexc !== -1) {
+            const theLength = searchedCart.productos.length
+            if (theLength > 0) {
+                const indexp = searchedCart.productos.findindex(element => element.id === receive.id);
+                Cart[indexc].productos[indexp].cantidad = Cart[indexc].productos[indexp].cantidad + receive.cantidad;
+            } else {
+                newProduct.id = receive.producto.id;
+                newProduct.timestamp = receive.producto.timestamp;
+                newProduct.nombre = receive.pronombre;
+                newProduct.descripcion = receive.descripcion;
+                newProduct.codigo = receive.codigo;
+                newProduct.foto = receive.foto;
+                newProduct.precio = receive.precio;
+                newProduct.stock = receive.stock;
+                newProduct.cantidad = receive.cantidad;
+            }
+        
+            Cart.productos.push(newProduct);
+
+            //The array gets updated here
+            let array = [];
+
+            Cart.forEach((element) => {
+                array.push({
+                    timestamp: element.timestamp,
+                    productos: element.productos,
+                })
+            })
+
+            //carrito.txt file is replaced with the updated array
+            try {
+                await fs.promises.unlink('./src/files/carrito.txt');
+                try {
+                    await Cart.save(array);
+                    res.json({
+                        message: 'Modificacion exitosa',
+                        product: array
+                    })
+                }
+                catch (error) {
+                    res.json({
+                        message: 'No fue posible cargar los carritos en Cart.txt',
+                        error: error
+                    })
+                }
+            }
+            catch (error) {
+                res.json({
+                    message: 'No se pudo borrar el archivo carrito.txt',
+                    error: error
+                })
+            }
+        } else {
+            res.json({
+                message: 'carrito no encontrado'
+            })
+        }
+    }
+    catch (error) {
+        res.json({
+            message: 'Ha ocurrido un error al intentar recuperar la lista de carritos',
+            error: error
+        })
+    }
+
+})
+
+//This route removes the cart with the selected id
 routerCart.delete('/:id', async (req, res) => {
     const id = parseInt(req.params.id);
     console.log(id);
@@ -171,24 +247,24 @@ routerCart.delete('/:id', async (req, res) => {
         try {
             const removedProduct = await Cart.deleteById(id);
             if (removedProduct.length === 0) {
-                res.send({
+                res.json({
                     message: "El carrito solicitado no existe"
                 })
             } else {
-                res.send({
+                res.json({
                     message: "El carrito ha sido eliminado",
                     product: removedProduct
                 })
             }
         }
         catch (error) {
-            res.send({
+            res.json({
                 message: "El carrito no pudo ser eliminado",
                 error: error
             })
         }
     } else {
-        res.send({
+        res.json({
             message: "El id suministrado no es numerico"
         })
     }
